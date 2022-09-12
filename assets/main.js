2627fcc3d0fad2cd84c269ea0f254834404c3cdc
Ronis Xogum <60937516+xogumon@users.noreply.github.com>
2022-09-12 18:17:11 -0300

    :100:

--- assets/main.js
+++ assets/main.js
@@ -184,7 +184,7 @@ window.onload = async function () {
 		div.dataset.userName = data.username || data["display-name"];
 		div.dataset.userId = data["user-id"];
 		div.classList.add("message", data["message-type"]);
-		if (data.message.split(" ").some((word) => word === new RegExp(`^@?${channel}$`, "i"))) {
+		if (data.message.split(" ").some((word) => new RegExp(`^@?${channel}$`, "i").test(word))) {
 			div.classList.add("mention");
 		} else if (!lastMessage || lastMessage.classList.contains("even")) {
 			div.classList.add("odd");
@@ -195,9 +195,20 @@ window.onload = async function () {
 			div.classList.add("first-message");
 		}
 		if (data.hasOwnProperty("reply-parent-msg-id")) {
+			console.log(data);
 			const reply = document.createElement("div");
 			reply.classList.add("reply-parent");
-			reply.innerHTML = `<span class="reply-parent-user">${data["reply-parent-user-name"]}</span> <span class="reply-parent-message">${data["reply-parent-msg-body"]}</span>`;
+			reply.innerHTML = `
+			<svg type="color-fill-current" width="17px" height="17px" version="1.1" viewBox="0 0 16 16" x="0px" y="0px">
+				<g>
+					<path d="M5 6H7V8H5V6Z"></path>
+					<path d="M9 6H11V8H9V6Z"></path>
+					<path fill-rule="evenodd" clip-rule="evenodd" d="M8 14L10 12H13C13.5523 12 14 11.5523 14 11V3C14 2.44772 13.5523 2 13 2H3C2.44772 2 2 2.44772 2 3V11C2 11.5523 2.44772 12 3 12H6L8 14ZM6.82843 10H4V4H12V10H9.17157L8 11.1716L6.82843 10Z"></path>
+				</g>
+			</svg>
+			Respondendo a <span class="reply-parent-user">@${data["reply-parent-user"]}</span>:
+			<span class="reply-parent-message">${data["reply-parent-msg-body"]}</span>
+			`;
 			div.appendChild(reply);
 		}
 		div.style.borderLeftColor = data.color;
